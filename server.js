aa528ddda98c70b4661e719efdfa886213fd5dc8
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-09-30 10:31:40 -0400

    show route added and working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const pokemon = require('./models/pokemon.js')
+const pokemon = require('./models/pokemon')
 
 
 // Parse Request Bodies if Content-Type Header is: application/x-www-form-urlencoded
@@ -15,7 +15,11 @@ app.get('/pokemon', (req, res) => {
     
     
     // SHOW
-
+// SHOW ROUTES - GETS ONE FRUIT
+app.get("/pokemon/:indexOfPokemonArray", (req, res) => {
+    console.log(req.params.id)
+    res.render("show.ejs", {pokemons: pokemon[req.params.indexOfPokemonArray]});
+  });
     
 app.listen(3000, () => {
     console.log("app is listening on port 3000")
